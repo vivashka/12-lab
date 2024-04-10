bffa2730374b10b8ec9807d3ae0886d6c92985c3
Vladimir <vovac8908@gmail.com>
2024-04-10 18:27:55 +0500

    Feat (mylist): создание двусвязного списка

--- lab/MyList.cs
+++ lab/MyList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BaseClassEmoji;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,140 @@ using System.Threading.Tasks;
 
 namespace lab
 {
-    internal class MyList
+    internal class MyList<T> where T : IInit, ICloneable, new()
     {
+        Point<T>? beg = null;
+        Point<T>? end = null;
+
+        int _count = 0;
+
+        public int Count => _count;
+
+        public Point<T> MakeRandomData()
+        {
+            T data = new();
+            data.RandomInit();
+            return new Point<T>(data);
+        }
+
+        public MyList() { }
+
+        public MyList(int size)
+        {
+            if (size <= 0) throw new Exception("Размер меньше нуля");
+            beg = MakeRandomData();
+            end = beg;
+            for (int i = 1; i < size; i++)
+            {
+                T newItem = MakeRandomItem();
+                AddToEnd(newItem);
+            }
+        }
+
+        public MyList(T[] collection)
+        {
+            if (collection == null) throw new Exception("Размер меньше нуля");
+            
+            if (collection.Length == 0) throw new Exception("Коллекция пуста");
+
+            T newData = (T)collection.Clone();
+            beg = new Point<T>(newData);
+            end = beg;
+
+            for (int i = 0; i < collection.Length; i++)
+            {
+                AddToEnd(collection[i]);
+            }
+        }
+
+        public Point<T> FindItem(T item)
+        {
+            Point<T> current = beg;
+            while (current != null)
+            {
+                if (current.Data == null)
+                    throw new Exception("Пустые данны");
+
+                if (current.Data.Equals(item))
+                    return current;
+
+                current = current.Next;
+            }
+            return null;
+        }
+
+        public bool RemoveItem(T item)
+        {
+            if (beg == null) throw new Exception("Пустой список");
+
+            Point<T>? pos = FindItem(item);
+
+            if (pos == null) return false;
+            _count--;
+
+            if (beg == end)
+            {
+                beg = end = null;
+                return true;
+            }
+
+            if (pos.Prev ==  null)
+            {
+                beg = beg?.Next;
+                beg.Next = null;
+                return true;
+            }
+
+            if (pos.Next == null)
+            {
+                end = end.Prev;
+                beg.Next = null;
+                return true;
+            }
+
+            Point<T> next = pos.Next;
+            Point<T> prev = pos.Prev;
+            pos.Next.Prev = prev;
+            pos.Prev.Next = next;
+            return true;
+        }
+
+        public T MakeRandomItem()
+        {
+            T data = new();
+            data.RandomInit();
+            return data;
+        }
+
+        public void AddToEnd(T item)
+        {
+            T newData = (T)item.Clone();
+            Point<T> newItem = new Point<T>(newData);
+            _count++;
+            if (end  != null)
+            {
+                end.Next = newItem;
+                newItem.Next = end;
+                end = newItem;
+            }
+            else
+            {
+                beg = newItem;
+                end = beg;
+            }
+        }
+
+        public void PrintList()
+        {
+            if (_count == 0)
+                Console.WriteLine("Список пуст");
+            
+            Point<T> current = beg;
+            for (int i = 0; current != null; i++)
+            {
+                Console.WriteLine(current);
+                current = current.Next;
+            }
+        }
     }
 }
--- lab/Program.cs
+++ lab/Program.cs
@@ -1,25 +1,81 @@
 ﻿using System;
+using BaseClassEmoji;
+using LabLibrary;
 
 namespace lab
 {
     public class Program
     {
-        // Это тестовый метод. Его можно удалить из проекта.
-        /// <summary>
-        /// Возводит число в квадрат.
-        /// </summary>
-        /// <param name="number">Число для возведения в квадрат</param>
-        /// <returns>Квадрат числа</returns>
-        public static int GetSquare(int number)
-        {
-            return number * number;
-        }
+        static MyList<Emoji> mainList;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            int number = 4;
-            int squaredNumber = GetSquare(number);
-            Console.WriteLine(squaredNumber);
+            Menu();
+        }
+        public static void Menu()
+        {
+            string[] tasks = {"Сформировать двунаправленный список Emoji",
+            "Распечатать полученный список",
+            "Удалить из списка первый элемент с заданным именем",
+            "Добавить в список элементы с номерами 1, 3, 5 и т. д.",
+            "Выполнить глубокое клонирование списка",
+            "Удалить список из памяти",
+            "Выход"};
+
+            byte apply = Display(tasks);
+            do
+            {
+                switch (apply)
+                {
+                    case 0:
+                        Console.WriteLine("Задайте размерность списка целым положительным числом");
+                        uint size = LabLib.ExtensionDoWhile<uint>();
+                        mainList = new MyList<Emoji>((int)size);
+                        break;
+                }
+            } while (apply >= 0);
+        }
+
+        public static byte Display(string[] options)
+        {
+            byte selectedItem = 0;
+            ConsoleKeyInfo pressedKey;
+
+            do
+            {
+                ColorText(selectedItem, options);
+                pressedKey = Console.ReadKey();
+                if (pressedKey.Key == ConsoleKey.UpArrow)
+                {
+                    selectedItem = selectedItem > 0 ? selectedItem -= (byte)1 : selectedItem = (byte)(options.Length - 1);
+                }
+                if (pressedKey.Key == ConsoleKey.DownArrow)
+                {
+                    selectedItem = (byte)((selectedItem + 1) % options.Length);
+                }
+            } while (pressedKey.Key != ConsoleKey.Enter);
+
+            return selectedItem;
+        }
+
+        public static void ColorText(byte apply, string[] options)
+        {
+            Console.Clear();
+            for (byte i = 0; i < options.Length; i++)
+            {
+                string prefix;
+                if (apply == i)
+                {
+                    prefix = "<";
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    prefix = "";
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine(options[i] + prefix);
+            }
         }
     }
 }
