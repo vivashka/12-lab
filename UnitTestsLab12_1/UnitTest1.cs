16eff8d3824d475b56508a7da32c71063478aa19
VCher_A <vovac8098@gmail.com>
2024-05-10 20:16:42 +0500

    Fix

--- UnitTestsLab12_1/UnitTest1.cs
+++ UnitTestsLab12_1/UnitTest1.cs
@@ -7,11 +7,38 @@ namespace UnitTestsLab12
     public class UnitTest1
     {
         [TestMethod]
-        public void TestCreation()
+        public void TestConstructorLength()
         {
             MyList<Emoji> myList = new MyList<Emoji>(10);
+            
+            Assert.IsTrue(myList.Count == 10);
+        }
+
+        [TestMethod]
+        public void TestConstructorCopy()
+        {
+            MyList<Emoji> myList = new MyList<Emoji>();
+            Emoji[] arrEmojis = new Emoji[10];
+
+            for (int i = 0; i < arrEmojis.Length; i++)
+            {
+                arrEmojis[i] = new Emoji();
+                arrEmojis[i].RandomInit();
+                myList.AddToEnd(arrEmojis[i]);
+            }
 
             Assert.IsTrue(myList.Count == 10);
+        }
+
+        [TestMethod]
+        public void TestFindItem()
+        {
+            MyList<Emoji> myList = new MyList<Emoji>();
+
+            Emoji expected = new Emoji();
+            myList.AddToEnd(expected);
+            
+            Assert.AreEqual(myList.FindItem("smile").Data.Tag, expected.Tag);
         }
     }
 }
